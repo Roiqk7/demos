ec148da0c390df691ac635f84915140781f92ff4
Roiqk7 <112021405+Roiqk7@users.noreply.github.com>
2022-11-17 13:44:32 +0100

    final tweaks: cosmetics and simplification
    This is indeed the final version

--- moveQueen.cpp
+++ moveQueen.cpp
@@ -1,33 +1,31 @@
-// * g++ -std=c++17 test.cpp -o test -I/Users/roiqk/SFML/include -L/Users/roiqk/SFML/build/lib  -lsfml-graphics -lsfml-window -lsfml-system
+// Example of command to run this code. For more info visit official SFML website
+//* g++ -std=c++17 moveQueen.cpp -o moveQueen -I/Users/roiqk/SFML/include -L/Users/roiqk/SFML/build/lib  -lsfml-graphics -lsfml-window -lsfml-system
 
 
 #include <array>        
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-//  map related
-#define N 8                 //  size of one side of the square map
-#define TILE_SIZE 100       //  size of one tile on the map
-#define QUEEN_SCALE 0.13    //  scale used for queen to fit into one tile
-#define DOT_SIZE 10         //  size of dot which highlights tiles queen can move to
+// map related
+#define N 8                 // size of one side of the square map
+#define TILE_SIZE 100       // size of one tile on the map
+#define QUEEN_SCALE 0.13    // scale used for queen to fit into one tile
+#define DOT_SIZE 10         // size of dot which highlights tiles queen can move to
 
-//  window related
-#define WIDTH N*TILE_SIZE    //  window width
-#define HEIGHT N*TILE_SIZE   //  window height
+// window related
+#define WIDTH N*TILE_SIZE   // window width
+#define HEIGHT N*TILE_SIZE  // window height
+
+// cosmetics
+#define LIGHT_GREY sf::Color{192, 192, 192, 127}
+#define PATH_TO_QUEEN_TEXTURE "/assets/goldQueen.png"
 
 // return values
 #define SUCCESS 0
 #define ERROR -1
 
-//  colors
-#define LIGHT_GREY sf::Color{192, 192, 192, 127}
-
 // create the window     
 sf::RenderWindow window(sf::VideoMode(sf::Vector2u(WIDTH, HEIGHT)), "Move Queen");
-
-//  constant values describing board
-const int QUEEN_NUM = 1;
-const int EMPTY_NUM = 0;
 
 
 /*
@@ -41,7 +39,7 @@ const int EMPTY_NUM = 0;
 */
 
 
-//  class which represents queen
+// class which represents queen
 class Queen {
     public:  
         sf::Texture goldQueenTexture;
@@ -52,26 +50,25 @@ class Queen {
         Queen(int &row, int &col) {
             this->row = row;
             this->col = col;
-            if (!goldQueenTexture.loadFromFile("/Users/roiqk/Desktop/Programming/C:C++/C++/NumeroDuo/Assets/goldQueen.png")) std::exit(0);
+            queenSprite.setScale(sf::Vector2f(QUEEN_SCALE, QUEEN_SCALE));
+            if (!goldQueenTexture.loadFromFile(PATH_TO_QUEEN_TEXTURE)) std::exit(0);
+            queenSprite.setTexture(goldQueenTexture);
         } 
 
         void render() {
             queenSprite.setPosition (sf::Vector2f(col*TILE_SIZE, row*TILE_SIZE));
-            queenSprite.setScale(sf::Vector2f(QUEEN_SCALE, QUEEN_SCALE));
-            queenSprite.setTexture(goldQueenTexture);
             window.draw(queenSprite);
         }
 };
 
 
-//  Board stores all important data and renders the actual board seen by the user
+// Board stores all important data and renders the actual board seen by the user
 class Board {
     public:
-        std::array<std::array<sf::RectangleShape, N>, N> boardTiles;    
+        std::array<std::array<sf::RectangleShape, N>, N> boardTiles;     
         sf::Vector2i mousePos;
         sf::CircleShape dot;
         bool highlight;
-        int black;        
 
         Board() {
             highlight = false;
@@ -85,34 +82,32 @@ class Board {
             }
         }
 
-        //  draws the board
+        // draws the board
         void render()
         {
-            black = 0;
             for (int row = 0; row < N; row++) {
                 for (int col = 0; col < N; col++) {
-                    if (black % 2) boardTiles[row][col].setFillColor(sf::Color::Black);
+                    if ((row + col) % 2) boardTiles[row][col].setFillColor(sf::Color::Black);
                     else boardTiles[row][col].setFillColor(sf::Color::White);
                     window.draw(boardTiles[row][col]);
-                    black++; 
                 }
-                black++;
             }
         }
 
-        //  highlights tiles queen can move to
+        // highlights tiles onto queen can move 
         void highlightTiles(Queen &queen)
         {
             for (int row = 0; row < N; row++) {
                 for (int col = 0; col < N; col++) {
+                    // prevents drawing dot over queen
                     if (row == queen.row && col == queen.col) continue;
-                    //  rows and cols
+                    //rows and cols
                     if (queen.row == row || queen.col == col) {
                         dot.setPosition(sf::Vector2f(col*TILE_SIZE+TILE_SIZE/2-DOT_SIZE, 
                                                      row*TILE_SIZE+TILE_SIZE/2-DOT_SIZE));
                     }
-                    //  diagonals
-                    if (checkQueenDiagonals(row, col, queen)) {
+                    // diagonals
+                    else if (checkQueenDiagonals(row, col, queen)) {
                         dot.setPosition(sf::Vector2f(col*TILE_SIZE+TILE_SIZE/2-DOT_SIZE, 
                                                      row*TILE_SIZE+TILE_SIZE/2-DOT_SIZE));
                     }
@@ -121,7 +116,7 @@ class Board {
             }
         }
 
-        //  detects click and based on where user clicked (un)highlights tiles
+        // detects click and based on where user clicked (un)highlights tiles
         void clicked(Queen &queen)
         {
             mousePos = sf::Mouse::getPosition(window);
@@ -134,26 +129,26 @@ class Board {
         }
 
     private: 
-        //  checks if user clicked on queen
+        // checks if user clicked on queen
         bool clickedOnQueen(Queen &queen)
         {
             return (compareMousePosAndNum(mousePos.x, queen.col) && compareMousePosAndNum(mousePos.y, queen.row));
         }
 
-        //  checks if user clicked on tile queen can move to thus is highlighted
+        // checks if user clicked on tile queen can move to thus is highlighted
         void clickedOnHighlighted(Queen &queen)
         {
-            //  rows
+            // rows
             if (compareMousePosAndNum(mousePos.y, queen.row)) {
                 queen.col = mousePos.x / 100;
                 return;
             }
-            //  cols
+            // cols
             if (compareMousePosAndNum(mousePos.x, queen.col)) {
                 queen.row = mousePos.y / 100;
                 return;
             }
-            //  diagonals 
+            // diagonals 
             for (int row = 0; row < N; row++) {
                 for (int col = 0; col < N; col++) {
                     if (checkQueenDiagonals(row, col, queen)) {
@@ -167,20 +162,21 @@ class Board {
             }
         }
 
-        //  decides if tile is on queen's diagonals
+        // decides if tile is on queen's diagonals
         bool checkQueenDiagonals(int &row, int &col, Queen &queen)
         {
             return (row + col == queen.row + queen.col || row - col == queen.row - queen.col);
         }   
 
-        //  decides if user clicked on certain tile
-        bool compareMousePosAndNum(int mousePos, int &num) {
+        // decides if user clicked on certain tile
+        bool compareMousePosAndNum(int &mousePos, int &num) {
             return (mousePos > num * TILE_SIZE && mousePos < num * TILE_SIZE + TILE_SIZE);
         }
 };
 
 
-void sfml(Board &board, Queen &queen);                        //  draws on the screen
+void sfml(Board &board, Queen &queen);          // handles gui
+void doEveryFrame(Board &board, Queen &queen);  // needs to happen every frame
 
 
 /*
@@ -194,7 +190,7 @@ void sfml(Board &board, Queen &queen);                        //  draws on the s
 */
 
 
-//  starts the program
+// starts the program
 int main()
 {
     int startingRow = 0, startingCol = 4;
@@ -204,19 +200,6 @@ int main()
     sfml(board, queen);
      
     return SUCCESS;
-}
-
-
-void doEveryFrame(Board &board, Queen &queen)
-{
-    window.clear();
-
-    board.render();
-    queen.render();
-
-    if (board.highlight) board.highlightTiles(queen);
-
-    window.display();
 }
 
 
@@ -231,7 +214,7 @@ void doEveryFrame(Board &board, Queen &queen)
 */
 
 
-//  visualizes the algorithm
+// visualizes the algorithm
 void sfml(Board &board, Queen &queen)
 {
     window.setFramerateLimit (60);
@@ -247,4 +230,15 @@ void sfml(Board &board, Queen &queen)
         doEveryFrame(board, queen);
     }
     return;
+}
+
+
+// helper function which is executed for every frame
+void doEveryFrame(Board &board, Queen &queen)
+{
+    window.clear();
+    board.render();
+    queen.render();
+    if (board.highlight) board.highlightTiles(queen);
+    window.display();
 }
